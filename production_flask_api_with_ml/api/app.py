942980447602f5be008074f6ac2fe2e598e9d210
Brandy K <brandy_1175@Hotmail.com>
2021-07-01 22:28:48 -0500

    1028pm 7-1

---
+++ production_flask_api_with_ml/api/app.py
@@ -0,0 +1,85 @@
+import requests
+
+from flask import Flask, json, request
+from flask import jsonify, render_template
+import datetime as dt
+import os
+import pandas as pd
+import sqlalchemy
+from pickle import load
+from pickle import dump
+
+from sqlalchemy import sql
+import numpy as np
+
+
+# from sklearn.model_selection import train_test_split
+# from sklearn.preprocessing import StandardScaler
+# from sklearn.ensemble import RandomForestClassifier
+
+# if not os.path.exists("data.db"):
+#     engine=sqlalchemy.create_engine('sqlite:///data.db')
+#     df=pd.read_csv("Resources/Merged_All_df.csv")
+#     df.to_sql("dataset",index=False,con=engine)
+
+# else:
+#     engine=sqlalchemy.create_engine('sqlite:///data.db')
+
+movie_directors_df = pd.read_csv('Resources/movie_director_df(1).csv')
+
+# model = load(open('model.pkl', 'rb'))
+# X_scaler = load(open('scaler.pkl', 'rb'))
+audience_score = [] 
+app = Flask(__name__)
+
+
+@app.route('/hello')
+def hello_world():
+    return 'Hello, Worldz!'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route('/directors')
+def getDirectors():
+    data_list = list(movie_directors_df.columns)
+    data_list.pop(0)
+    return jsonify(data_list)
+
+@app.route("/predict", methods=['POST'])
+def handlePredict():
+    return request.form
+
+
+# @app.route("/predict/<Pregnancies>/<Glucose>/<BloodPressure>/<SkinThickness>/<Insulin>/<BMI>/<DiabetesPedigreeFunction>/<Age>")
+# def predict(Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age):
+#     new_data = np.array([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+#     return jsonify(target_names[model.predict(X_scaler.transform(new_data))[0]])
+
+# @app.route("/add/<Pregnancies>/<Glucose>/<BloodPressure>/<SkinThickness>/<Insulin>/<BMI>/<DiabetesPedigreeFunction>/<Age>/<Outcome>")
+# def add_data(Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age,Outcome):
+#     sql_str = f"INSERT INTO dataset VALUES ({Pregnancies},{Glucose},{BloodPressure},{SkinThickness},{Insulin},{BMI},{DiabetesPedigreeFunction},{Age},{Outcome});"
+#     engine.execute(sql_str)
+#     return jsonify("success")
+
+# @app.route("/train")
+# def train():
+#     sql_str="SELECT Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age, Outcome FROM dataset"
+#     df = pd.read_sql(sql_str,engine)
+#     target = df["Outcome"]
+#     data = df.drop("Outcome", axis=1)
+#     X_train, X_test, y_train, y_test = train_test_split(data, target, random_state=42)
+#     X_scaler = StandardScaler().fit(X_train)
+#     X_train_scaled = X_scaler.transform(X_train)
+#     X_test_scaled = X_scaler.transform(X_test)
+#     rf = RandomForestClassifier(n_estimators=420)
+#     model = rf.fit(X_train_scaled, y_train)
+#     acc=model.score(X_test_scaled, y_test)
+#     dump(X_scaler, open('scaler.pkl', 'wb'))
+#     dump(model, open('model.pkl', 'wb'))
+
+    # return jsonify({"acc":acc,})
+
+if __name__ == '__main__':
+    app.run()
---
+++ production_flask_api_with_ml/api/static/index.js
@@ -0,0 +1,82 @@
+var form = d3.select("#form");
+
+// Create event handlers for clicking the button or pressing the enter key
+// button.on("click", runEnter);
+form.on("submit", runEnter);
+var predictedResult = '';
+// Create the function to run for both events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    var genre = d3.select("#genre").property("value");
+    var director = d3.select("#director").property("value");
+    var content_rating = d3.select("#content_rating").property("value");
+    var production_company = d3.select("#production_company").property("value");
+    var runtime = d3.select("#runtime").property("value");
+    // var BMI = d3.select("#BMI").property("value");
+    // var DiabetesPedigreeFunction = d3.select("#DiabetesPedigreeFunction").property("value");
+    // var Age = d3.select("#Age").property("value");
+    d3.json(`/predict/${genre}/${director}/${content_rating}/${production_company}/${runtime}`).then(d => {
+        d3.select("#result").text(`the model predicts your ${d}`);
+        predictedResult = d;
+    });
+
+}
+
+var director = []
+
+d3.json("/directors").then((data) => {
+    console.log(data)
+
+    var checkbox = d3.selectAll('#Director_Checks');
+    counter = 0;
+    data.forEach(function (director) {
+        divel = checkbox.append('div')
+        .classed('col-md-3', true);
+        divel.append('input')
+        .property('value', director)
+        .property('name', director)
+        .property('id', `cb${counter}`)
+        .property('type', 'checkbox')
+        .classed('form-check-input', true);
+        divel.append('label')
+        .text(director)
+        .property('for', `cb${counter}`)
+        .classed('form-check-label', true);
+        counter = counter+1; 
+    });
+
+});
+
+// function correct() {
+//     d3.event.preventDefault();
+//     var ans=[] 
+//     outcome = ans.indexOf(predictedResult)
+//     updateData(outcome)
+// }
+
+
+// function wrong() {
+//     d3.event.preventDefault();
+//     var ans=[ "positive","negative"] 
+//     outcome = ans.indexOf(predictedResult)
+//     updateData(outcome)
+// }
+// function updateData(outcome) {
+
+//     var Pregnancies = d3.select("#Pregnancies").property("value");
+//     var Glucose = d3.select("#Glucose").property("value");
+//     var BloodPressure = d3.select("#BloodPressure").property("value");
+//     var SkinThickness = d3.select("#SkinThickness").property("value");
+//     var Insulin = d3.select("#Insulin").property("value");
+//     var BMI = d3.select("#BMI").property("value");
+//     var DiabetesPedigreeFunction = d3.select("#DiabetesPedigreeFunction").property("value");
+//     var Age = d3.select("#Age").property("value");
+//     d3.json(`/add/${Pregnancies}/${Glucose}/${BloodPressure}/${SkinThickness}/${Insulin}/${BMI}/${DiabetesPedigreeFunction}/${Age}/${outcome}`).then(d=>console.log(d));
+
+// }
+
+// d3.select('#yes').on("click",correct)
+// d3.select('#no').on("click",wrong)
