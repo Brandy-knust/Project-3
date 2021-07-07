9189f585cf97ed84e94dd56304f8ca49c61004c9
Brandy K <brandy_1175@Hotmail.com>
2021-07-06 22:30:36 -0500

    1030pm 7-6

--- production_flask_api_with_ml/api/app.py
+++ production_flask_api_with_ml/api/app.py
@@ -1,4 +1,5 @@
 from typing_extensions import runtime
+from numpy.lib.twodim_base import triu_indices
 import requests
 
 from flask import Flask, json, request
@@ -22,7 +23,7 @@ tensorflow.keras.__version__
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -41,9 +42,9 @@ else:
     engine=sqlalchemy.create_engine('sqlite:///data.db')
 
 movie_directors_df = pd.read_csv('Resources/movie_director_df(1).csv')
-#dim_df.csv = pd.read_csv('Resources/dim_df.csv')
 
-model = load(open('model.pkl', 'rb'))
+
+model = load_model('model.h5')
 #X_scaler = load(open('scaler.pkl', 'rb'))
 audience_score = [] 
 app = Flask(__name__)
@@ -63,23 +64,47 @@ def getDirectors():
     data_list.pop(0)
     return jsonify(data_list)
 
-@app.route("/predict", methods=['POST'])
-def handlePredict():
-#    return request.form
-    print(request.form)
-    return render_template("index.html", result = 67) 
+# @app.route("/predict", methods=['POST'])
+# def handlePredict():
+# #    return request.form
+#     
+#     print(request.form)
+#     return render_template("index.html", result = 67) 
     
 
-# @app.route("/predict",  methods=['POST'])
-# def predict():
-#     input_dictionary = request.form
-#     # genre = input_dictionary["Genre"]
-#     # production_company = input_dictionary["Production Company"]
-#     # content_rating = input_dictionary["rating"]
-#     # runtime = input_dictionary["runtime"]
-#     # new_data = np.array([[genre,director,content_rating,production_company,runtime]])
-#     app.logger.info(f'new_data : {new_data}')
-#     return jsonify(model.predict(new_data))
+@app.route("/predict",  methods=['POST'])
+def predict():
+    input_dictionary = request.form
+    dim_df = pd.read_csv('Resources/dim_df.csv')
+    data=list(input_dictionary.items())
+    response_array=np.array(data)
+
+   
+
+    for i in range(0, len(response_array)):
+        if response_array[i,1] == '<=90 minutes':
+            response_array[i,1]= response_array[i,1].replace('<=90 minutes','1.0')
+        elif response_array[i,1] == '90-120 minutes':
+            response_array[i,1]= response_array[i,1].replace('90-120 minutes','2.0')
+        elif response_array[i,1] == '>=120 minutes':    
+            response_array[i,1]= response_array[i,1].replace('>=120 minutes','3.0')
+    dummy_list = np.zeros(len(dim_df.columns))
+
+    for i in range(0, len(response_array)):
+        index = dim_df.columns.get_loc(response_array[i,1])
+        dummy_list[index] = 1
+
+    app.logger.info(f'data : {data}')
+
+    # director_list = input_dictionary.keys()[4:]
+    model_result = model.predict(dummy_list.reshape(1,-1))
+    app.logger.info(model_result)
+    # rebuilt_dict = {"rating" : input_dictionary[0], "genre" : input_dictionary[1], "prod_co": input_dictionary[2], "run_time" : input_dictionary[3], "director": director_list}
+    result= {'input':input_dictionary,'score':model_result[0][0]}
+    # result= {'input':input_dictionary[:4],'score':model_result[0][0], 'director': director_list}
+    app.logger.info(result)
+    #return jsonify(model.predict(new_data))
+    return render_template("index.html", result = result)
 
 # @app.route("/add/<Director>/<genre>/<runtime>/<production_ompany>/<content_rating>/<outcome>")
 # def add_data(Director,genre,runtime,production_company,content_rating,outcome):
@@ -106,4 +131,4 @@ def handlePredict():
 #    return jsonify({"acc":acc,})
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
